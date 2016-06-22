9cfec057ef929349a5dc5bced530e48cfc9691d3
Emil Nilsson <emil_nilsson_96@live.se>
2016-06-22 23:59:22 +0200

    Update class.groups.php

--- class.groups.php
+++ class.groups.php
@@ -2,10 +2,25 @@
 
 class vGroups
 {
-	protected $groups = [];
-	private $defaultGroup = 1;
+	protected $groups        = [];
+	protected $specialGroups = [];
+	private $defaultGroup    = 1;
 
-	public function __construct($groups)
+	public function __construct()
+	{
+	}
+
+	public function getGroups()
+	{
+		return $this->groups;
+	}
+
+	public function getSpecialGroups()
+	{
+		return $this->specialGroups;
+	}
+
+	public function addGroups($groups)
 	{
 		try
 		{
@@ -34,8 +49,32 @@ class vGroups
 		}
 	}
 
-	public function getGroups()
+	public function addSpecialGroups($specialGroups)
 	{
-		return $this->groups;
+		try
+		{
+			if(!is_array($specialGroups))
+				throw new Exception("Parameter \$specialGroups must be a type of an array");
+
+			foreach($specialGroups as $groupKey => $group)
+			{
+				if(strpos($group, "#") !== false)
+				{
+					list($group, $description) = explode("#", $group);
+
+					$this->specialGroups[trim($group)] =
+					[
+						"special_group" => $groupKey,
+						"description"   => trim($description)
+					];
+				}
+				else
+					$this->specialGroups[trim($group)] = ["special_group" => $groupKey, "description" => "No description for this group"];
+			}
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage() . "<hr><b>Trace:</b> " . $e->getTraceAsString());
+		}
 	}
 }
